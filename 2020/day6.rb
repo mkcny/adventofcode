6438c05e9c5dce756802d2f4c5057ea0343f5273
Michael Cooney <michael.cooney@shopify.com>
2020-12-09 14:24:00 -0500

    day 6 part 2

--- 2020/day6.rb
+++ 2020/day6.rb
@@ -1,4 +1,14 @@
+# Part 1
+
 puts File.read("day6-input")
   .split("\n\n")
   .map { _1.tr("\n", '').chars.uniq.count }
+  .sum
+
+
+# Part 2
+
+puts File.read("day6-input")
+  .split("\n\n")
+  .map { _1.lines.map(&:chomp).map(&:chars).reduce(&:&).count }
   .sum
