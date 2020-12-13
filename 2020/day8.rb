1c995c4c0d4926597dc7887ddc6b5dd709b504c1
Michael Cooney <michael.cooney@shopify.com>
2020-12-13 10:09:30 -0500

    cleaning up a bit before part 2

--- 2020/day8.rb
+++ 2020/day8.rb
@@ -3,37 +3,53 @@ require 'sorbet-runtime'
 
 extend T::Sig
 
-sig { params(instructions: T::Array[String]).returns(Integer) }
+class Instruction < T::Struct
+  prop :command, String
+  const :value, Integer
+end
+
+class Result < T::Struct
+  const :acc, Integer
+  const :run, T::Array[Integer]
+end
+
+sig { params(instructions: T::Array[Instruction]).returns(Result) }
 def execute_instructions(instructions)
   acc = 0
   index = 0
   run = T.let([], T::Array[Integer])
 
   while instruction = instructions[index]
-    cmd, value = T.must(instruction.match(/([a-z]{3}) ([0-9+\-]+)/)).captures
     break if run.include?(index)
     run << index
 
-    #binding.pry
-    #puts instruction
-
-    case cmd
+    case instruction.command
     when "nop"
       index = index + 1
     when "acc"
       index = index + 1
-      acc = acc + value.to_i
+      acc = acc + instruction.value
     when "jmp"
-      index = index + value.to_i
+      index = index + instruction.value
     end
   end
 
-  acc
+  puts "acc: #{acc}, run: #{run}"
+
+  Result.new(acc: acc, run: run)
+end
+
+sig { params(lines: T::Array[String]).returns(T::Array[Instruction]) }
+def parse_instructions(lines)
+  lines.map { |line|
+    cmd, value = T.must(line.match(/([a-z]{3}) ([0-9+\-]+)/)).captures
+    Instruction.new(command: T.must(cmd), value: value.to_i)
+  }
 end
 
 
 if __FILE__ == $0
-  instructions = File.read("input/day8").lines
+  instructions = parse_instructions(File.read("input/day8").lines)
   result = execute_instructions(instructions)
   puts "part 1 result: #{result}"
 end
--- 2020/test/day8_test.rb
+++ 2020/test/day8_test.rb
@@ -14,8 +14,26 @@ class Day8Tests < Minitest::Test
       "jmp -4",
       "acc +6"
     ]
+    result = execute_instructions(parse_instructions(instructions))
+    assert_equal 5, result.acc
+  end
+
+  def test_example_input_part_2
+    instructions = [
+      "nop +0",
+      "acc +1",
+      "jmp +4",
+      "acc +3",
+      "jmp -3",
+      "acc -99",
+      "acc +1",
+      "jmp -4",
+      "acc +6"
+    ]
+    instructions = parse_instructions(instructions)
+    instructions[7].command = "nop"
     result = execute_instructions(instructions)
-    assert_equal 5, result
+    assert_equal 8, result.acc
   end
 
 end
