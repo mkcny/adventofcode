52871dd031dea605c009a1d865c1a8360d91e23d
Michael Cooney <michael.cooney@shopify.com>
2020-12-22 13:58:16 -0500

    day 10 part 2 - passing tests, but too slow

--- 2020/day10.rb
+++ 2020/day10.rb
@@ -4,12 +4,11 @@ extend T::Sig
 
 sig { params(jolts: T::Array[Integer]).returns(Integer) }
 def day10_part1(jolts)
-  jolts.unshift(0)
   results = []
 
   jolts.each_index do |index|
     current = T.must(jolts[index])
-    nxt = jolts[index + 1] || (current + 3)
+    nxt = jolts[index + 1] || break
 
     results << nxt - current
   end
@@ -18,7 +17,28 @@ def day10_part1(jolts)
   result.fetch(1) * result.fetch(3)
 end
 
+sig { params(jolts: T::Array[Integer], start_index: Integer, level: Integer).returns(Integer) }
+def count_all_paths(jolts, start_index = 0, level = 0)
+  count = 1
+  (start_index...jolts.length).each do |index|
+    range_start = index + 2
+    range_end = index + 3
+    range_end = jolts.length - 1 if range_end >= jolts.length
+
+    (range_start..range_end).each do |i|
+      diff = jolts.fetch(i) - jolts.fetch(index)
+      if diff > 1 && diff <= 3
+        result = count_all_paths(jolts, i, level + 1)
+        count = count + result
+      end
+    end
+  end
+  return count
+end
+
 if __FILE__ == $0
-  input = File.read("input/day10").lines.map(&:to_i).sort
-  puts day10_part1(input)
+  input = File.read("input/day10").lines.map(&:to_i).sort.unshift(0)
+  input.push(T.must(input.last) + 3)
+  puts "part 1: #{day10_part1(input)}"
+  puts "part 2: #{count_all_paths(input)}"
 end
--- 2020/test/day10_test.rb
+++ 2020/test/day10_test.rb
@@ -3,21 +3,31 @@ require "minitest/autorun"
 require_relative "../day10"
 
 class Day10Tests < Minitest::Test
-  def test_part1
-    input = [
-      16,
-      10,
-      15,
-      5,
-      1,
-      11,
-      7,
-      19,
-      6,
-      12,
-      4
+  def setup
+    @input = [
+      0,  # 0
+      1,  # 1
+      4,  # 2
+      5,  # 3
+      6,  # 4
+      7,  # 5
+      10, # 6
+      11, # 7
+      12, # 8
+      15, # 9
+      16, # 10
+      19, # 11
+      22  # 12
     ]
-    result = day10_part1(input.sort)
+  end
+
+  def test_part1
+    result = day10_part1(@input)
     assert_equal 35, result
+  end
+
+  def test_part2
+    assert_equal 1, count_all_paths(@input, 7)
+    assert_equal 8, count_all_paths(@input)
   end
 end
