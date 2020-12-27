30c25ad3060dd34039469ece2562629d80605c7e
Michael Cooney <michael.cooney@shopify.com>
2020-12-27 11:24:27 -0500

    day 1 part 2 in elixir. Working, but not elegant.

--- 2020/day1.ex
+++ 2020/day1.ex
@@ -1,12 +1,28 @@
 defmodule Day1 do
   def combinations([h|t]), do: (for x <- t, do: [h, x]) ++ combinations(t)
   def combinations([]), do: []
+
+  def combine_thrice(list) do
+    for x <- list, y <- list, z <- list, do: [x, y, z]
+  end
 end
 
-File.read!("input/day1")
-  |> String.split("\n", trim: true)
+# Part 1
+
+File.stream!("input/day1")
+  |> Stream.map(&String.trim/1)
   |> Enum.map(&String.to_integer/1)
   |> Day1.combinations
+  |> Enum.find(&(Enum.sum(&1) == 2020))
+  |> Enum.reduce(&*/2)
+  |> IO.inspect
+
+# Part 2
+
+File.stream!("input/day1")
+  |> Stream.map(&String.trim/1)
+  |> Enum.map(&String.to_integer/1)
+  |> Day1.combine_thrice
   |> Enum.find(&(Enum.sum(&1) == 2020))
   |> Enum.reduce(&*/2)
   |> IO.inspect
