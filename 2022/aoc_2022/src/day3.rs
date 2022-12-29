49603e127eed396e5b3780e46112f0801d1595a8
Michael Cooney <mike.cooney@gmail.com>
2022-12-29 11:59:44 -0500

    day 3 cleanup

--- 2022/aoc_2022/src/day3.rs
+++ 2022/aoc_2022/src/day3.rs
@@ -5,16 +5,11 @@ pub fn part1() {
         .unwrap()
         .split("\n")
         .map(|line| line.split_at(line.len() / 2))
-        .map(|(a, b)| find_common_char(a, b))
+        .map(|(a, b)| find_common_char(vec![a, b]))
         .map(|c| get_priority(c))
         .sum();
 
     println!("d3 p1: {}", sum);
-}
-
-fn find_common_char(a: &str, b: &str) -> char {
-    let set: HashSet<char> = a.chars().collect();
-    b.chars().find(|x| set.contains(x)).unwrap()
 }
 
 fn get_priority(c: char) -> u32 {
@@ -26,21 +21,22 @@ pub fn part2() {
     let sum: u32 = std::fs::read_to_string("inputs/day3")
         .unwrap()
         .split("\n")
-        .map(|line| line.chars().collect::<HashSet<char>>())
-        .collect::<Vec<HashSet<char>>>()
+        .collect::<Vec<&str>>()
         .array_chunks::<3>()
-        .map(|triple| {
-            let intersection = &triple[0]
-                .intersection(&triple[1])
-                .copied()
-                .collect::<HashSet<char>>()
-                .intersection(&triple[2])
-                .copied()
-                .collect::<HashSet<char>>();
-            intersection.iter().next().unwrap().clone()
-        })
+        .map(|triple| find_common_char(triple.to_vec()))
         .map(|c| get_priority(c))
         .sum();
 
     println!("d3 p2: {}", sum);
+}
+
+fn find_common_char(strs: Vec<&str>) -> char {
+    strs.iter()
+        .map(|s| s.chars().collect::<HashSet<char>>())
+        .reduce(|acc, e| acc.intersection(&e).copied().collect::<HashSet<char>>())
+        .unwrap()
+        .iter()
+        .next()
+        .unwrap()
+        .clone()
 }
